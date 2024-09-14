d99fffb4f83db2ea6642610c04cadc92c2443544
Pat Patterson <pat@backblaze.com>
2024-09-14 13:18:01 -0700

    Updates to tie in with blog post

--- app.py
+++ app.py
@@ -5,15 +5,22 @@ import os
 import sys
 from functools import wraps
 from http import HTTPStatus
+import locale
 from shutil import copyfileobj
 from time import perf_counter
 from traceback import print_exception
 from zipfile import ZipFile, ZipInfo, ZIP_DEFLATED
 
 import psutil
+from dotenv import load_dotenv
 from flask import Flask, Response, request, abort
 from flask_executor import Executor
 from s3fs import S3FileSystem
+
+# Auto-set locale for thousands separators in debug output
+locale.setlocale(locale.LC_ALL, '')
+
+load_dotenv()
 
 logging.basicConfig()
 logger = logging.getLogger(os.path.basename(__file__))
@@ -36,6 +43,9 @@ if not input_bucket_name:
     raise Exception("Input bucket name is not defined.")
 if not output_bucket_name:
     raise Exception("Output bucket name is not defined.")
+
+DEFAULT_COPY_BUFFER_SIZE = 1024 * 1024  # 1 MiB
+copy_buffer_size = os.environ.get('COPY_BUFFER_SIZE', default=DEFAULT_COPY_BUFFER_SIZE)
 
 b2fs = S3FileSystem(version_aware=True)
 
@@ -78,7 +88,7 @@ def auth_required(view_function):
 
 
 @executor.job
-def make_zip_job(input_files, output_file):
+def make_zip_job(selected_filenames, zip_filename):
     # noinspection PyBroadException
     try:
         """
@@ -86,31 +96,33 @@ def make_zip_job(input_files, output_file):
         """
         start_time = perf_counter()
         bytes_read = 0
-        output_path = f'{output_bucket_name}/{output_file}'
+        zip_file_path = f'{output_bucket_name}/{zip_filename}'
 
-        logger.debug(f'Opening {output_path} for writing as a ZIP')
-        with b2fs.open(output_path, 'wb') as f, ZipFile(f, 'w') as zipfile:
-            for input_file in input_files:
-                input_path = f'{input_bucket_name}/{input_file}'
+        logger.debug(f'Opening {zip_file_path} for writing as a ZIP')
+        with b2fs.open(zip_file_path, 'wb') as f, ZipFile(f, 'w') as zipfile:
+            for filename in selected_filenames:
+                input_path = f'{input_bucket_name}/{filename}'
                 logger.debug(f'Writing {input_path} to ZIP')
 
-                # Get file info, so we have a timestamp for the ZIP entry and the file size
-                # for calculating compression ratio
                 try:
-                    input_file_info = b2fs.info(input_path)
-                    logger.debug(f'Input file info: {input_file_info}')
-                    last_modified = input_file_info['LastModified']
-                    bytes_read += input_file_info['size']
+                    # Get file info, so we have a timestamp for the ZIP entry and the file size
+                    # for calculating compression ratio
+                    file_info = b2fs.info(input_path)
+                    logger.debug(f'Input file info: {file_info}')
+                    last_modified = file_info['LastModified']
+                    bytes_read += file_info['size']
                     date_time = (last_modified.year, last_modified.month, last_modified.day,
                                  last_modified.hour, last_modified.minute, last_modified.second)
 
-                    zipinfo = ZipInfo(filename=input_file, date_time=date_time)
+                    zipinfo = ZipInfo(filename=filename, date_time=date_time)
                     # You need to set the compress_type on each ZipInfo object - it is not inherited from the ZipFile!
                     zipinfo.compress_type = ZIP_DEFLATED
+                    # Since we know the file size, set it so that large files work correctly
+                    zipinfo.file_size = file_info['size']
 
                     with (b2fs.open(input_path, 'rb') as src,
                           zipfile.open(zipinfo, 'w') as dst):
-                        copyfileobj(src, dst)
+                        copyfileobj(src, dst, copy_buffer_size)
 
                     logger.debug(f'Wrote {input_path} to ZIP')
                 except FileNotFoundError:
@@ -118,13 +130,13 @@ def make_zip_job(input_files, output_file):
 
         # Don't get the output file info unless we're going to log statistics!
         if logger.isEnabledFor(logging.DEBUG):
-            output_file_info = b2fs.info(output_path)
+            output_file_info = b2fs.info(zip_file_path)
             bytes_written = output_file_info['size']
             compression_ratio = bytes_written / bytes_read if bytes_read != 0 else 0
             memory_usage_in_mb = psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2
 
-            logger.debug(f'Finished writing {output_path} in {perf_counter() - start_time:.3f} seconds.')
-            logger.debug(f'Read {bytes_read} bytes, wrote {bytes_written} bytes, '
+            logger.debug(f'Finished writing {zip_file_path} in {perf_counter() - start_time:.3f} seconds.')
+            logger.debug(f'Read {bytes_read:n} bytes, wrote {bytes_written:n} bytes, '
                          f'compression ratio was {compression_ratio * 100:.0f}%')
             logger.debug(f'Currently using {memory_usage_in_mb:.0f} MB')
 
@@ -158,4 +170,5 @@ def make_zip_endpoint():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT', '5000')
+    app.run(port=port)
